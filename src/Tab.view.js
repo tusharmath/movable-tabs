0d0ff585a1c77bd393d3114ecb5cca53d8d6e0f6
Imam <imamudin.naseem@olacabs.com>
2016-08-17 15:49:38 +0530

    fixup! chore(jsx): adding jsx support

--- src/Tab.view.js
+++ src/Tab.view.js
@@ -9,14 +9,19 @@ import R from 'ramda'
 const mapI = R.addIndex(R.map)
 
 export default ({$}) => {
-  const sliderEL = <div className="slider"></div>
-  const navListEL = mapI((k, i) => <li key={k} className={i === 0 ? 'active' : ''}
+  const sliderEL = <div className='slider'></div>
+  const navListEL = mapI((k, i) => <li className={i === 0 ? 'active' : ''}
                                        onclick={R.partial($.__onNavClick, [i])}>{k}</li>, $.__navItems)
   const children =
-    <div className="movable-tabs">
-      <div className="header">
-        <ul className="nav">{navListEL}</ul>
+    <div className='movable-tabs'>
+      <div className='header'>
+        <ul className='nav'>{navListEL}</ul>
         {sliderEL}
+      </div>
+      <div className='pane'>
+        <div className='pane-container' style={{width: `${$.__navItems.length * 100}%`}}>
+          {$.__paneItems}
+        </div>
       </div>
     </div>
   return {children, sliderEL, navListEL}
