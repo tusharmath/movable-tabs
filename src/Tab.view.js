28dd9951a373d99c6c9755aa043ef649a3b98559
Tushar Mathur <tusharmath@gmail.com>
2016-08-17 17:29:55 +0530

    refactor(tab): break view into smaller functions

--- src/Tab.view.js
+++ src/Tab.view.js
@@ -8,10 +8,30 @@ import R from 'ramda'
 
 const mapI = R.addIndex(R.map)
 
+export const createPanelContainer = ({$}) => {
+  const width = `${$.__navItems.length * 100}%`
+  return (
+    <div className='pane-container' style={{width}}>
+      {$.__paneItems}
+    </div>
+  )
+}
+
+export const createNavList = ({$}) => {
+  const className = i => i === 0 ? 'active' : ''
+  const LI = (k, i) => <li
+    className={className(i)}
+    onclick={R.partial($.__onNavClick, [i])}
+  >
+    {k}
+  </li>
+  return mapI(LI, $.__navItems)
+}
+
 export default ({$}) => {
   const sliderEL = <div className='slider'></div>
-  const navListEL = mapI((k, i) => <li className={i === 0 ? 'active' : ''}
-                                       onclick={R.partial($.__onNavClick, [i])}>{k}</li>, $.__navItems)
+  const navListEL = createNavList({$})
+  const paneContainerEL = createPanelContainer({$})
   const children =
     <div className='movable-tabs'>
       <div className='header'>
@@ -19,9 +39,7 @@ export default ({$}) => {
         {sliderEL}
       </div>
       <div className='pane'>
-        <div className='pane-container' style={{width: `${$.__navItems.length * 100}%`}}>
-          {$.__paneItems}
-        </div>
+        {paneContainerEL}
       </div>
     </div>
   return {children, sliderEL, navListEL}
